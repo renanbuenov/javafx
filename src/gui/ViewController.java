9581343302c6ac542e743342b76eebc6ba7f1a9f
renanbuenov <renanbuenov@hotmail.com>
2021-10-10 15:15:27 -0300

    Contraints created

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,15 +1,19 @@
 package gui;
 
+import java.net.URL;
 import java.util.Locale;
+import java.util.ResourceBundle;
 
 import gui.util.Alerts;
+import gui.util.Constraints;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
 
-public class ViewController {
+public class ViewController implements Initializable{
 	
 	@FXML
 	private TextField txtNumber1;
@@ -36,5 +40,13 @@ public class ViewController {
 			Alerts.showAlert("Error", "Parse Error", e.getMessage(), AlertType.ERROR);
 		}
 		
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		Constraints.setTextFieldDouble(txtNumber1);
+		Constraints.setTextFieldDouble(txtNumber2);		
+		Constraints.setTextFieldMaxLength(txtNumber1, 12);
+		Constraints.setTextFieldMaxLength(txtNumber2, 12);
 	}
 }
